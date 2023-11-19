31e4eaf71a81a8a002ec630d65393273c061199f
whatsgoodg <rlwjddl1596@naver.com>
2023-11-19 14:31:31 +0000

    change from composition to inheritance, license added at head of source code

--- avl_tree.h
+++ avl_tree.h
@@ -1,49 +1,82 @@
+/*
+File: avl_tree.h
+Copyright (c) 2023 김기정
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <iostream>
 #ifndef AVLTREE_H
-#define AVLTREE_H	
+#define AVLTREE_H
 
 // Define Set
 class AVLTree { 
-private:
-	friend class Set;
-	// AVL Tree is composed of Nodes
-	struct Node {
-	public:
-		// members
-		int key_;
-		Node* left_; // left child
-		Node* right_; // right child
-		int height_; 
-		int depth_;
+protected:
+        // AVL Tree is composed of Nodes
+        struct Node {
+        public:
+                // members
+                int key_;
+                Node* left_; 
+                Node* right_; 
+                int height_; 
+                int depth_;
 
-		// Node constructor
-		Node(int key) {
-			key_ = key;
-			left_ = right_ = NULL;
-			height_ = depth_ = -1;
-		}
-	};
+                // Node constructor
+                Node(int key) {
+                        key_ = key;
+                        left_ = right_ = NULL;
+                        height_ = depth_ = -1;
+                }
+        };
 
-private:
-	AVLTree();
-	Node* Insert_Node(Node* cur_node, int key); // for insert
-	Node* Find_Node(int key); // for Get methods except for Get_Size
-	Node* Get_Rank(Node* cur_node); // for Get Rank
-	Node* Erase(Node* cur_node, int key); // for Erase
-	Node* Single_Right_Rotation(Node* cur_node);
-	Node* Double_Right_Rotation(Node* cur_node);
-	Node* Single_Left_Rotation(Node* cur_node);
-	Node* Double_Left_Rotation(Node* cur_node);
-	int Set_Height(); // for Erase and Insert to maintain AVL Tree structure
-	int Get_Height(); // for Erase and Insert to maintain AVL Tree structure
-	int Set_Depth(); // Get rank of node
-	int Get_Depth(); // Set rank of node	
+protected:
+		//AVLTree constructor
+        AVLTree();
+		// insert a node
+        Node* insert_Node(Node* cur_node, int key);
+		// Find node matcheds a key 
+        Node* find_Node(int key); 
+		// Get Rank by a given node
+        int find_Rank(Node* cur_node);
+		// Erase a node matches a given key
+        Node* erase(Node* cur_node, int key); 
+		// Single righr rotation
+        Node* single_Right_Rotation(Node* cur_node);
+		// double right rotation
+        Node* double_Right_Rotation(Node* cur_node);
+		// single left rotation
+        Node* single_Left_Rotation(Node* cur_node);
+		// double left rotation
+        Node* double_Left_Rotation(Node* cur_node);
+		// set height of a given node
+        void set_Height(Node* cur); 
+		// get height of a given node
+        int get_Height(Node* cur); 
+		// set dpeth of a given node
+        void set_Depth(Node* cur); 
+		// get depth of a given node
+        int get_Depth(Node* cur); 
 
-private:
-	// members
-	Node* root_; // root node
-	size_t num_of_nodes_; // size of Set
+protected:
+        // members
+        Node* root_; 
+        size_t num_of_nodes_; 
 };
 #endif
-
-
--- set.h
+++ set.h
@@ -1,3 +1,25 @@
+/*
+File: set.h
+Copyright (c) 2023 김기정
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #include "avl_tree.h"
 
 #ifndef SET_H
@@ -5,21 +27,24 @@
 
 // Define Set
 class Set { 
-
 public:
-	Set(); // Set constructor
-	int Insert(int key);// insert a node storing key and print depth of it
-	int Erase(int key); // erase a node storing key and print depth of it 
-	int Find(int key); // print depth of a node storing key
-	int Get_Rank(int key); // print rank and depth of a node storing key
-	int Get_Min_Key(int key); // get a min key of node from a subtree whose root is a node storing key
-	int Get_Max_Key(int key); // get a max key of node from a subtree whose root is a node storing key
-	int Get_Size(); // get the number of nodes in Set
-	bool Is_Empty(); // get true if Set is empty
-
-private:
-	// members
-	AVLTree avl_tree_;
+	// Set constructor
+	Set();
+	// insert a node has a given key
+	int insert(int key);
+	// erase a node has a given key
+	int erase(int key); 
+	// find depth of a node has a given key
+	int find(int key); 
+	// get a rank of a node has a given key
+	int get_rank(int key); 
+	// get a node which has a minimum key of subtree where its root has a given key
+	int get_min_key(int key); 
+	// get a node which has a maximum key of subtree wh`ere its root has a given key
+	int get_max_key(int key); 
+	// get a size of a whole avl tree
+	int get_size(); 
+	// return whether avl tree is emptry or not
+	bool is_empty(); 
 };
 #endif
-
